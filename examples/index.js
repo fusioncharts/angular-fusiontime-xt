92ca0836b47e345c4f3c1a33dffd855e7c0d0ef7
Jimut Dhali <jemdhali67@gmail.com>
2017-04-10 15:34:02 +0530

    Added eslint

--- examples/index.js
+++ examples/index.js
@@ -1,17 +1,16 @@
 import angular from 'angular';
-import "../src/angular.fusiontime";
-import tsData from "./data/datarepo";
+import '../src/angular.fusiontime';
+import tsData from './data/datarepo';
 
-const app = angular.module("myApp", ["ng-fusiontime"]);
+const app = angular.module('myApp', ['ng-fusiontime']);
 
-app.controller("myCtrl", ["$scope", function ($scope) {
+app.controller('myCtrl', ['$scope', $scope => {
   $scope.tsData = tsData;
   $scope.charts = Object.keys(tsData);
   // Init
   $scope.fcDataSource = $scope.tsData[$scope.charts[0]].data;
 
-  $scope.onLinkClick = function (item) {
+  $scope.onLinkClick = item => {
     $scope.fcDataSource = $scope.tsData[item].data;
-  }
-
+  };
 }]);
--- src/angular.fusiontime.js
+++ src/angular.fusiontime.js
@@ -1,92 +1,91 @@
-(function() {
+((function () {
+  var clone = function (obj) {
+    var temp = obj.constructor();
 
-	var clone = function(obj) {
-		if (obj === null || typeof(obj) !== 'object' || 'isActiveClone' in obj)
-			return obj;
-		var temp = obj.constructor();
-		for (var key in obj) {
-			if (Object.prototype.hasOwnProperty.call(obj, key)) {
-				obj['isActiveClone'] = null;
-				temp[key] = clone(obj[key]);
-				delete obj['isActiveClone'];
-			}
-		}
-		return temp;
-	};
+    if (obj === null || typeof obj !== 'object' || 'isActiveClone' in obj) {
+      return obj;
+    }
 
-	var chart = null;
+    Object.keys(obj).forEach(function (key) {
+      obj.isActiveClone = null;
+      temp[key] = clone(obj[key]);
+      delete obj.isActiveClone;
+    });
 
-	var fc = angular.module('ng-fusiontime', []);
+    return temp;
+  };
 
-	fc.directive('fusiontime', function() {
-		return {
-			scope: {
-				width: '@',
-				height: '@',
-				datasource: '@'
-			},
+  var chart = null;
 
-			link: function(scope, element, attrs) {
-				var chartConfig = {
-					type: "timeseries",
-					width: attrs.width,
-					height: attrs.height,
-					renderAt: element[0],
-					dataFormat: 'json',
-					dataSource: clone(scope.$parent.fcDataSource)
-				};
+  var fc = angular.module('ng-fusiontime', []);
 
-				var setDataTimer,
-					setChartData = function(dataSource) {
-						chartConfig.dataSource = dataSource;
-						// clear previous dataUpdate timer
-						if (setDataTimer) {
-							clearTimeout(setDataTimer);
-						}
+  fc.directive('fusiontime', function () {
+    return {
+      scope: {
+        width: '@',
+        height: '@',
+        datasource: '@'
+      },
 
-						setDataTimer = setTimeout(function() {
-							if (chart && chart.setJSONData) {
-								//  TODO: rerender the chart with new Data
-							}
-						}, 100);
-					},
+      link: function (scope, element, attrs) {
+        var chartConfig = {
+          type: 'timeseries',
+          width: attrs.width,
+          height: attrs.height,
+          renderAt: element[0],
+          dataFormat: 'json',
+          dataSource: clone(scope.$parent.fcDataSource)
+        };
 
-					drawChart = function(dataSource) {
+        var setDataTimer;
+        var setChartData = function (dataSource) {
+          chartConfig.dataSource = dataSource;
+          // clear previous dataUpdate timer
+          if (setDataTimer) {
+            clearTimeout(setDataTimer);
+          }
 
-						chartConfig.dataSource = clone(dataSource);
-						chart = new FusionCharts(chartConfig);
-						chart.render();
+          setDataTimer = setTimeout(function () {
+            if (chart && chart.setJSONData) {
+              //  TODO: rerender the chart with new Data
+            }
+          }, 100);
+        };
 
-					};
+        var drawChart = function (dataSource) {
+          chartConfig.dataSource = clone(dataSource);
+          chart = new FusionCharts(chartConfig);
+          chart.render();
+        };
 
-				// attach a ovserver on dataSource
-				// $observe is used for only interpolation like ==> {{ exp }}
-				scope.$watch('datasource', function(newVal, oldVal) {
-					if (scope.$parent.fcDataSource) {
-						drawChart(scope.$parent.fcDataSource);
-					}
-				});
+        // attach a ovserver on dataSource
+        // $observe is used for only interpolation like ==> {{ exp }}
+        scope.$watch('datasource', function () {
+          if (scope.$parent.fcDataSource) {
+            drawChart(scope.$parent.fcDataSource);
+          }
+        });
 
-				scope.$watch('width', function(newVal, oldVal) {
-					if (newVal !== oldVal) {
-						// TODO: resize width
+        scope.$watch('width', function (newVal, oldVal) {
+          if (newVal !== oldVal) {
+            // TODO: resize width
 
-					}
-				});
+          }
+        });
 
-				scope.$watch('height', function(newVal, oldVal) {
-					if (newVal !== oldVal) {
-						// TODO: resize height
+        scope.$watch('height', function (newVal, oldVal) {
+          if (newVal !== oldVal) {
+            // TODO: resize height
 
-					}
-				});
+          }
+        });
 
-				angular.element(document).ready(function() {
-					if (scope.$parent.fcDataSource) {
-						drawChart(scope.$parent.fcDataSource);
-					}
-				});
-			}
-		}
-	});
-})();
+        angular.element(document).ready(function () {
+          if (scope.$parent.fcDataSource) {
+            drawChart(scope.$parent.fcDataSource);
+          }
+        });
+      }
+    };
+  });
+})());
